b9e4cddee36cf0068cca87e84a3d6c13b3d8c7bc
Charlie Groves <cgroves@statestitle.com>
2018-07-18 17:25:10 -0700

    Add some comments

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
 import collections
 import pymssql
-
 from contextlib import contextmanager
+
 from dataclasses import dataclass, field, fields
+
 from settings import (
     RESWARE_DATABASE_NAME, RESWARE_DATABASE_PASSWORD, RESWARE_DATABASE_PORT,
-    RESWARE_DATABASE_SERVER, RESWARE_DATABASE_USER, ACTION_LIST_DEF_ID
+    RESWARE_DATABASE_SERVER, RESWARE_DATABASE_USER
 )
+
+
 @contextmanager
 def _connect_to_db():
     with pymssql.connect(
@@ -21,6 +24,7 @@ def _connect_to_db():
 
 
 def tableclass(table, lookup=None, one_to_many=False, **kwargs):
+    """Marks a dataclass as loadable from a specified SQL table"""
     def wrap(cls, lookup=lookup):
         dataclass(cls, **kwargs)
         cls.table = table
@@ -44,6 +48,7 @@ def tableclass(table, lookup=None, one_to_many=False, **kwargs):
 
 
 def col(name, parser=None, nullable=False):
+    """Marks a tableclass field as coming rom the named column on the table of the tableclass"""
     metadata = {'column': name, 'nullable': nullable}
     if parser:
         metadata['parser'] = parser
@@ -51,13 +56,16 @@ def col(name, parser=None, nullable=False):
 
 
 class ColumnMissing(Exception):
+    """Raised when the col for a field on a tableclass isn't in a fetched row"""
     pass
+
 
 class ParsingFailed(Exception):
+    """Raised when the parser on a col raises an exception on a value found in a row for the col name"""
     pass
 
 
-def parse_col(dclass, field, row):
+def _parse_col(dclass, field, row):
     if field.metadata['column'] not in row:
         raise ColumnMissing(f'{dclass} field {field.name} expected a column named {field.metadata["column"]} in the row {row}')
     val = row[field.metadata['column']]
@@ -75,22 +83,32 @@ def parse_col(dclass, field, row):
         raise ParsingFailed(f'{dclass} field {field.name} parser {parser} blew up on "{val}"') from e
 
 
-def create_from_db(dclass, row):
-    return dclass(*[parse_col(dclass, f, row) for f in fields(dclass) if 'column' in f.metadata])
+def _create_from_db(dclass, row):
+    return dclass(*[_parse_col(dclass, f, row) for f in fields(dclass) if 'column' in f.metadata])
 
 
-def load(conn, cls):
-    columns = ', '.join([f.metadata['column'] for f in fields(cls) if 'column' in f.metadata])
-    query = f'SELECT {columns} FROM {cls.table}'
+def load(conn, tablecls):
+    """Returns a dict of the lookup of tablecls to instances of it for all rows in its table
+
+    If one_to_many is set on the tableclass, the returned dict will be from the key on the instance to a list of
+    instances with that same key
+
+    If lookup is specified, it's expected to be a fieldname or a tuple of fieldnames to create the keys for the returned
+    dictionary
+
+    If lookup isn't specified, it's assumed to be 'id'"""
+
+    columns = ', '.join([f.metadata['column'] for f in fields(tablecls) if 'column' in f.metadata])
+    query = f'SELECT {columns} FROM {tablecls.table}'
     results = {}
-    if cls.one_to_many:
+    if tablecls.one_to_many:
         results = collections.defaultdict(list)
     with conn.cursor() as cursor:
         cursor.execute(query)
         for r in cursor.fetchall():
-            instance = create_from_db(cls, r)
-            key = cls.create_key(instance)
-            if cls.one_to_many:
+            instance = _create_from_db(tablecls, r)
+            key = tablecls.create_key(instance)
+            if tablecls.one_to_many:
                 results[key].append(instance)
             else:
                 assert key not in results, f"Was expecting a single item for {key} but got {instance} and {results[key]}"
